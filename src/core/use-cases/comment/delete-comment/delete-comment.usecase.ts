5c4d13d4fa8ebd9685f74531341b3195e6ae1aaf
unknown <aquie00t@icloud.com>
2026-03-28 18:56:36 +0300

    fix(comment): update delete usecase to use unit of work transaction

--- src/core/use-cases/comment/delete-comment/delete-comment.usecase.ts
+++ src/core/use-cases/comment/delete-comment/delete-comment.usecase.ts
@@ -1,32 +1,38 @@
-import type { ICommentRepository } from "@core/ports/repositories/comment.repository";
+import type { TransactionPort } from "@core/ports/services/transaction.port";
 import type { CachePort } from "@core/ports/services/cache.port";
 import { NotFoundError, ForbiddenError } from "@core/errors";
 import type { DeleteCommentInput } from "./delete-comment-usecase.input";
 
 export class DeleteCommentUseCase {
     constructor(
-        private readonly commentRepository: ICommentRepository,
+        private readonly transactionService: TransactionPort,
         private readonly redisService: CachePort,
     ) {}
 
     async execute(input: DeleteCommentInput): Promise<void> {
-        const comment = await this.commentRepository.findById(input.commentId);
-
-        if (!comment) {
-            throw new NotFoundError("Comment not found.");
-        }
-
-        if (comment.authorId !== input.userId) {
-            throw new ForbiddenError("This post is not yours.");
-        }
-
-        if (comment.postId !== input.postId) {
-            throw new ForbiddenError(
-                "This comment does not belong to the mentioned post.",
+        await this.transactionService.runInTransaction(async (ctx) => {
+            const comment = await ctx.commentRepository.findById(
+                input.commentId,
             );
-        }
 
-        await this.commentRepository.delete(input.commentId, input.postId);
+            if (!comment) {
+                throw new NotFoundError("Comment not found.");
+            }
+
+            if (comment.authorId !== input.userId) {
+                throw new ForbiddenError("This comment is not yours.");
+            }
+
+            if (comment.postId !== input.postId) {
+                throw new ForbiddenError(
+                    "This comment does not belong to the mentioned post.",
+                );
+            }
+
+            await ctx.commentRepository.delete(input.commentId);
+
+            await ctx.postRepository.decrementCommentsCount(input.postId);
+        });
 
         await this.redisService.deleteByPattern("posts:feed:*");
     }
